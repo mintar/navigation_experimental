CNURobotics/sbpl_lattice_planner

draft create version and test file

python/mprim/test.py
@@ -1,5 +1,7 @@
 
 #from genmprim_unicycle import *
 #genmprim_unicycle("test.mprim", True)
-from genmprim_unicycleplussideways import *
-genmprim_unicycleplussideways("test.mprim", True)
+#from genmprim_unicycleplussideways import *
+#genmprim_unicycleplussideways("test.mprim", True)
+from genmprim_create import *
+genmprim_create("create.mprim", True)

mintar/navigation_experimental

sbpl_lattice_planner/python/mprim/test.py
@@ -1,5 +1,7 @@
 
 #from genmprim_unicycle import *
 #genmprim_unicycle("test.mprim", True)
-from genmprim_unicycleplussideways import *
-genmprim_unicycleplussideways("test.mprim", True)
+#from genmprim_unicycleplussideways import *
+#genmprim_unicycleplussideways("test.mprim", True)
+from genmprim_create import *
+genmprim_create("create_050.mprim", True)